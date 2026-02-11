66da04f3d7bc61e77170829145de2faa4b48f9a9
Navya jain <navyajainn28@gmail.com>
2026-02-11 10:41:03 +0530

    Add files via upload

---
+++ Calling procedure.sql
@@ -0,0 +1,16 @@
+-- Declare
+--   x int := 100000;
+--   y int:= 0;
+
+-- begin
+--     procedure4(x,y);
+--     dbms_output.put_line('Return of out in y varaible:'||' '|| y);
+-- end;
+
+-- Declare
+--   x int := 100000;
+
+-- begin
+--     p1(x);
+--     dbms_output.put_line('Return:'||' '|| x);
+-- end;
